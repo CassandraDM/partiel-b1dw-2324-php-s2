5786d1733d1641b9651bbe95f60f848c700c2be2
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-06-17 16:39:29 +0200

    update style

--- src/post.php
+++ src/post.php
@@ -10,6 +10,7 @@
     </div>
 </div>
 
+<div class="post-list-content">
 <form method="post" action="">
     <select name="sort" id="sort">
         <option value="asc">Sort by ascending (prix)</option>
@@ -50,14 +51,21 @@
         <div class="post-header">
             <h2>Football</h2>
         </div>
-            <h3><?= $post['categorie'] ?></h3>
-            <h3><?= $post['groupe'] ?></h3>
-            <p><?= $post['equipe1'] ?></p>
-            <p><?= $post['equipe2'] ?></p>
-            <p><?= $post['date_heure'] ?></p>
-            <p><?= $post['lieu'] ?></p>
-            <p><?= $post['prix'] ?></p>
-            <p><?= $post['description'] ?></p>
+            <div class="post-content">
+                <div class="post-row">
+                    <div class="post-element">            
+                        <h3>Catégorie :</h3>
+                        <p><?= $post['categorie'] ?></p>
+                    </div>
+                    <div class="post-element">            
+                        <h3>Groupe :</h3>
+                        <p><?= $post['groupe'] ?></p>
+                    </div>
+                </div>
+                <p><?= $post['equipe1'] ?> / <?= $post['equipe2'] ?></p>
+                <p><?= $post['date_heure'] ?> | <?= $post['lieu'] ?> | <?= $post['prix'] ?> €</p>
+                <p><?= $post['description'] ?></p>
+            </div>
             <div class="post-actions">
                 <a href="scripts/delete-post-script.php?id=<?= $post['id'] ?>" class="action"><i class="fa-solid fa-trash"></i></a>
                 <a href="edit-post.php?id=<?= $post['id'] ?>" class="action"><i class="fa-solid fa-edit"></i></a>
@@ -67,7 +75,7 @@
     </div>
     <?php endforeach ?>
 </div>
-
+</div>
 <?php
     require_once __DIR__ . '/parts/footer.php'
 ?>
